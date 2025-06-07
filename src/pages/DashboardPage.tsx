793744dbae457c3f5e82a9cabaebf7da8b67409d
ArtHard <1artem1nazarov1@gmail.com>
2025-06-07 22:45:21 +0300

    fix: remove background color from DashboardPage

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -13,7 +13,7 @@ const DashboardPage = () => {
 		return <p>Загрузка пользователя...</p>;
 
 	return (
-		<div className="min-h-full flex items-center bg-slate-900 text-white py-12 px-6">
+		<div className="min-h-full flex items-center text-white py-12 px-6">
 			<div className="max-w-6xl w-full mx-auto space-y-10">
 				<div className="text-center">
 					<h1 className="text-3xl font-bold mb-2">

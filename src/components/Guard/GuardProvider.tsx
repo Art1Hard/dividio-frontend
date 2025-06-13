ee99b0e45671af396225c96350b10b0938aff3bf
hardov <1artem1nazarov1@gmail.com>
2025-06-13 21:15:20 +0300

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- src/components/Guard/GuardProvider.tsx
+++
@@ -1,9 +0,0 @@
-import { useInitAuth } from "@src/hooks/auth/useInitAuth";
-import { type JSX } from "react";
-
-const GuardProvider = ({ children }: { children: JSX.Element }) => {
-	useInitAuth();
-	return children;
-};
-
-export default GuardProvider;

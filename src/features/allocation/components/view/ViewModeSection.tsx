8dcef5581cd41c65a8d4c371e1be855b184392e4
hardov <1artem1nazarov1@gmail.com>
2025-06-27 00:32:34 +0300

    fix: remove unnecessary data prop from AllocationChart in ViewModeSection

--- src/features/allocation/components/view/ViewModeSection.tsx
+++ src/features/allocation/components/view/ViewModeSection.tsx
@@ -16,7 +16,7 @@ const ViewModeSection = ({
 			{!isChartOpen ? (
 				<AllocationList allocations={allocations} />
 			) : (
-				<AllocationChart data={allocations} />
+				<AllocationChart />
 			)}
 		</div>
 	);
